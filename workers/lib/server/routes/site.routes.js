d7a98cbde901ebe66811ace8f01f329eb65d649b
Parag More <paragmore306@gmail.com>
2026-04-20 21:38:02 +0530

    Add site effciency api routes

--- workers/lib/server/routes/site.routes.js
+++ workers/lib/server/routes/site.routes.js
@@ -6,7 +6,7 @@ const {
   AUTH_CAPS,
   AUTH_LEVELS
 } = require('../../constants')
-const { getSiteLiveStatus, getSiteOverviewGroupsStats } = require('../handlers/site.handlers')
+const { getSiteLiveStatus, getSiteOverviewGroupsStats, getSiteEfficiency } = require('../handlers/site.handlers')
 const { createCachedAuthRoute } = require('../lib/routeHelpers')
 
 module.exports = (ctx) => [
@@ -44,6 +44,25 @@ module.exports = (ctx) => [
       ['site-overview/groups'],
       ENDPOINTS.SITE_OVERVIEW_GROUPS,
       getSiteOverviewGroupsStats,
+      [`${AUTH_CAPS.m}:${AUTH_LEVELS.READ}`]
+    )
+  },
+  {
+    method: HTTP_METHODS.GET,
+    url: ENDPOINTS.SITE_EFFICIENCY,
+    schema: {
+      querystring: {
+        type: 'object',
+        properties: {
+          overwriteCache: { type: 'boolean' }
+        }
+      }
+    },
+    ...createCachedAuthRoute(
+      ctx,
+      ['site-efficiency'],
+      ENDPOINTS.SITE_EFFICIENCY,
+      getSiteEfficiency,
       [`${AUTH_CAPS.m}:${AUTH_LEVELS.READ}`]
     )
   }
