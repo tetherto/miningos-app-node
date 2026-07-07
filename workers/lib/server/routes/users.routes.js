a3e50c8f9837847a2bfba9f3ea3375c4c289abab
Hemant Tekwani <tekwani.hemant@gmail.com>
2026-07-08 01:22:50 +0530

    user perms update

--- workers/lib/server/routes/users.routes.js
+++ workers/lib/server/routes/users.routes.js
@@ -51,7 +51,7 @@ module.exports = (ctx) => [
     url: ENDPOINTS.USERS,
     onRequest: async (req, rep) => {
       await authCheck(ctx, req, rep)
-      await capCheck(ctx, req, rep, ['users:w'])
+      await capCheck(ctx, req, rep, ['users:r'])
     },
     handler: async (req, rep) => {
       const users = await listUsers(ctx, req, rep)
